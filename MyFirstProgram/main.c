a117f9fd0741480de6bcc5db588a97752bd433e1
Jonlin Pei <jpei@Macintosh-4.local>
2014-10-28 06:28:39 -0700

    Initial Commit

---
+++ MyFirstProgram/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyFirstProgram
+//
+//  Created by Jonlin Pei on 10/28/14.
+//  Copyright (c) 2014 Jonlin Pei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
