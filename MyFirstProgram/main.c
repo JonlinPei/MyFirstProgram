c422a089abe9018b1566680aa4313265444fc880
sanbei120 <jonlinpei@gmail.com>
2015-02-04 00:09:26 -0800

    update

--- MyFirstProgram/main.c
+++ MyFirstProgram/main.c
@@ -6,10 +6,14 @@
 //  Copyright (c) 2014 Jonlin Pei. All rights reserved.
 //
 
-#include <stdio.h>
+#include <stdio.h>  // Defines printf etc.
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int a, b, c;
+    
+    a = b = c = 23;
+    printf( "a = %d, b = %d, c = %d", a, b+1, c+2);
+    
     return 0;
 }
